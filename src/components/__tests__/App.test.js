b6c1f37768b223f08192043189d89260e87af44c
Ali Mujthaba <a.mujthaba321@gmail.com>
2018-10-06 13:55:16 +0500

    jest config for enzyme  completed and first enzyme test completed

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../App';
+import CommentBox from '../comment/CommentBox';
+
+// it('shows a comment box', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   console.log(div.innerHTML);
+//   expect(div.innerHTML).toContain('Comment Box');
+//   ReactDOM.unmountComponentAtNode(div);
+// });
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  console.log(div.innerHTML);
-  expect(div.innerHTML).toContain('Comment Box');
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
